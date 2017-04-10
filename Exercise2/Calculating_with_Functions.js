e095dd5c092c0876298b02696baf9936f3134db2
mohanakrishnag <mohanakrishna.g@hotmail.com>
2017-04-10 16:03:26 +0530

    Fixed the code review comments

--- Exercise2/Calculating_with_Functions.js
+++ Exercise2/Calculating_with_Functions.js
@@ -1,33 +1,18 @@
-/*Calculating with Functions*/
-function zero(operation) {
-    return operation ? operation(0) : 0;
-}
-function one(operation) {
-    return operation ? operation(1) : 1;
-}
-function two(operation) {
-    return operation ? operation(2) : 2;
-}
-function three(operation) {
-    return operation ? operation(3) : 3;
-}
-function four(operation) {
-    return operation ? operation(4) : 4;
-}
-function five(operation) {
-    return operation ? operation(5) : 5;
-}
-function six(operation) {
-    return operation ? operation(6) : 6;
-}
-function seven(operation) {
-    return operation ? operation(7) : 7;
-}
-function eight(operation) {
-    return operation ? operation(8) : 8;
-}
-function nine(operation) {
-    return operation ? operation(9) : 9;
+var zero = commonFunc(0);
+var one = commonFunc(1);
+var two = commonFunc(2);
+var three = commonFunc(3);
+var four = commonFunc(4);
+var five = commonFunc(5);
+var six = commonFunc(6);
+var seven = commonFunc(7);
+var eight = commonFunc(8);
+var nine = commonFunc(9);
+
+function commonFunc(value) {
+    return function (operation) {
+        return operation ? operation(value) : value;
+    }
 }
 
 function plus(value2) {
@@ -35,16 +20,19 @@ function plus(value2) {
         return value1 + value2;
     }
 }
+
 function minus(value2) {
     return function (value1) {
         return value1 - value2;
     }
 }
+
 function times(value2) {
     return function (value1) {
         return value1 * value2;
     }
 }
+
 function dividedBy(value2) {
     return function (value1) {
         return value1 / value2;
